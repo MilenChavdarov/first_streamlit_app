88853a37a49cacf02bf0f17eb12d9b3c178fc393
mchavdarov@positivethinking.tech <Pr0p0li$1>
2022-11-24 15:01:06 +0200

    Modify the fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,10 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado toast')
 
 my_fruit_list = p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-st.dataframe(my_fruit_list)
+
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+st.dataframe(fruits_to_show)
