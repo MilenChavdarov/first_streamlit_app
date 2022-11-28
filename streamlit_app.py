851ee30ccf59beb17fb52fd3275078f961e87528
Milen Chavdarov <mendu@abv.bg>
2022-11-28 10:58:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,9 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 st.text("Hello from Snowflake:")
 st.text(my_data_row)
+
+my_curr.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+st.text("The fruit list contains::")
+st.text(my_data_row)
+
