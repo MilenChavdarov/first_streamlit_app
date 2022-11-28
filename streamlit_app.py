1bb53ade58b279c986283b5dd9f233fc15697380
mchavdarov@positivethinking.tech <Pr0p0li$1>
2022-11-28 14:49:32 +0200

    final changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,13 @@ import requests as r
 import snowflake.connector
 from urllib.error import URLError
 
+def get_fruityvice_data(this_fruit_choise):
+    st.write('The user entered ', fruit_choice)
+    fruityvice_response = r.get("https://fruityvice.com/api/fruit/" + this_fruit_choise)
+    fruityvice_normalized = p.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+
 st.title("Breakfast Favourites")
 
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -26,29 +33,26 @@ try:
     if not fruit_choice:
         st.error("Select a food to get information!")
     else:
-        st.write('The user entered ', fruit_choice)
-        fruityvice_response = r.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        # write your own comment -what does the next line do? 
-        fruityvice_normalized = p.json_normalize(fruityvice_response.json())
-        # write your own comment - what does this do?
-        st.dataframe(fruityvice_normalized)        
+        result_fruitvice = get_fruityvice_data(fruit_choice)
+        st.dataframe(result_fruitvice)        
 except URLError as e:
     st.error(e)
 
-st.stop()
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+#st.stop()
+if st.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
+    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    my_data_row = my_cur.fetchone()
+    st.text("Hello from Snowflake:")
+    st.text(my_data_row)
 
-add_my_fruit = st.text_input("What fruit would you like to add?", 'Apple')
-st.write("The fruit " + add_my_fruit + " is added.")
+    add_my_fruit = st.text_input("What fruit would you like to add?", 'Apple')
+    st.write("The fruit " + add_my_fruit + " is added.")
 
-my_cur.execute("insert into fruit_load_list values('from streamlit " + add_my_fruit + "')")
+    my_cur.execute("insert into fruit_load_list values('from streamlit " + add_my_fruit + "')")
 
-my_cur.execute("select * from FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-st.text("The fruit list contains:")
-st.text(my_data_rows)
+    my_cur.execute("select * from FRUIT_LOAD_LIST")
+    my_data_rows = my_cur.fetchall()
+    st.text("The fruit list contains:")
+    st.text(st.dataframe(my_data_rows))
